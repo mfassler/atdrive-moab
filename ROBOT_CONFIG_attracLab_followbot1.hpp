c09793fbd4ec19d16fa76b3d1ac73e661e80651a
Mark Fassler <mfassler@users.noreply.github.com>
2019-08-21 17:29:07 +0900

    new IP addresses

--- ROBOT_CONFIG_attracLab_followbot1.hpp
+++ ROBOT_CONFIG_attracLab_followbot1.hpp
@@ -1,17 +1,16 @@
 #ifndef __ROBOT__CONFIG__HPP
 #define __ROBOT__CONFIG__HPP
 
+// This is the "followbot" prototype
 
-#define _MOAB_IP_ADDRESS "192.168.53.199"
+#define _MOAB_IP_ADDRESS "192.168.17.201"
 #define _NETMASK "255.255.255.0"
-#define _DEFUALT_GATEWAY "192.168.53.1"
-#define _BROADCAST_IP_ADDRESS "192.168.53.255"
+#define _DEFUALT_GATEWAY "192.168.17.1"
+#define _BROADCAST_IP_ADDRESS "192.168.17.255"
 
-#define _AUTOPILOT_IP_ADDRESS "192.168.53.211"
+#define _AUTOPILOT_IP_ADDRESS "192.168.17.11"
 
 
-///////////////////////
-// This is the "followbot" prototype that I use in AttracLab:
 #define _STEERING_PW_CENTER 0.001424
 #define _STEERING_PW_RANGE 0.000391
 
