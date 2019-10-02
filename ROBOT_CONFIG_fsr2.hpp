a485aef91cf5bbacbf93b848db93533096aa891b
Mark Fassler <mfassler@users.noreply.github.com>
2019-10-02 13:27:30 +0900

    new settings for FSR2

--- ROBOT_CONFIG_fsr2.hpp
+++ ROBOT_CONFIG_fsr2.hpp
@@ -10,9 +10,22 @@
 #define _AUTOPILOT_IP_ADDRESS "192.168.32.222"
 
 
-#define _STEERING_PW_CENTER 0.001645
+#define _STEERING_PW_CENTER 0.001625
 #define _STEERING_PW_RANGE 0.000350
 
+#define IMU_STORED_CONFIG
+const char _IMU_CONFIG[] = {
+	// Config register:
+	0x55,
+
+	0xd4, 0xff, 0xb8, 0xff,
+	0xe5, 0xff, 0x22, 0x00,
+	0xd7, 0xff, 0x3c, 0xfe,
+	0xff, 0xff, 0x00, 0x00,
+	0x00, 0x00, 0xe8, 0x03,
+	0xa3, 0x01,
+
+};
 
 
 #endif // __ROBOT__CONFIG__HPP
